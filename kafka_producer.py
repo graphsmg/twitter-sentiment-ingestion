04493e5784d3a45d79935d71aeb8bfbd25d78f4b
graphsmg <graphs.mage@gmail.com>
2022-03-14 22:14:03 -0400

    json write on producer side, json read on consumer side

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,8 +1,11 @@
+import json
+
 from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
 for i in range(100):
-    r = producer.send(topic="test_topic", value=b"abc")
+    x = json.dumps({"v1": 1, "v2": 2})
+    r = producer.send(topic="test_topic", value=bytes(x, "utf-8"))
     print(r.is_done)
 
