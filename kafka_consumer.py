d2796726560e5c46666dd1b50375ccaac67d5819
graphsmg <graphs.mage@gmail.com>
2022-03-15 22:00:03 -0400

    adding twitter support

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,11 +1,12 @@
 import json
+import pprint
 
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer("test_topic", bootstrap_servers="localhost:9092")
+consumer = KafkaConsumer("recent_tweets_downloader", bootstrap_servers="localhost:9092")
 
 for msg in consumer:
     value = msg.value.decode("utf-8")
     x = json.loads(value)
-    print(x["v1"])
+    pprint.pprint(x)
 
